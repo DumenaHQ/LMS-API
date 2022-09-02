86cd93cf1cbbc05754cc0f8ffed9a573cc45e3af
Chibuzo <chibuzo.henry@gmail.com>
2022-09-02 10:49:47 +0100

    Update learner response data

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,7 @@ export const
         learner: 'learner'
     },
     USER_FIELDS = 'fullname email username role active_organization createdAt updatedAt',
-    LEARNER_FIELDS = 'parent school avatar resident_state',
+    LEARNER_FIELDS = 'parent school interests isUserOnboarded avatar resident_state',
     SALT_ROUNDS = 10,
     UPLOADS = {
         course_thumbs: 'course_thumbs',
