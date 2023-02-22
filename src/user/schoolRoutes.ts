a3525b145e06714bfffa4cd7562aaa061f25b6ed
Chibuzo <chibuzo.henry@gmail.com>
2023-02-22 18:34:15 +0100

    Remove learner for schools

--- src/user/schoolRoutes.ts
+++ src/user/schoolRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { isAuthenticated, isSchool, isSchoolOrAdmin } from "../middleware/verifyToken";
-import { addSchoolStudents, downloadSchoolStudents, listSchoolStudents } from "./controller";
+import { addSchoolStudents, downloadSchoolStudents, listSchoolStudents, removeChild } from "./controller";
 
 export const router = Router();
 
 router.get('/:id/learners', isAuthenticated, isSchool, listSchoolStudents);
 
 router.post('/:id/learners', isAuthenticated, isSchool, addSchoolStudents);
+
+router.delete('/:id/learners/:learnerid', isAuthenticated, isSchool, removeChild);
 
 router.get('/:id/download-students-list', downloadSchoolStudents);
