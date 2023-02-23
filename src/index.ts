a9d8337b0f19389f31176cddfec0dc65188d57f0
Chibuzo <chibuzo.henry@gmail.com>
2023-02-23 13:38:57 +0100

    Modify cors

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,7 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
 
 app.use(helmet());
-app.use(cors({
-    origin: process.env.BASE_URL
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
