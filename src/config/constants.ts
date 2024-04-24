5d7387c77082e9ff6349da4c274bd9d5bbd0e089
BEZALEEL NWABIA <bezaleelnwabia@gmail.com>
2024-04-24 17:19:25 +0100

    Added default values in the constant file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -26,4 +26,22 @@ export const
     TEMPLATE_FILE_PATH = 'downloads/templates',
     AWS_S3_REGION = 'us-east-1',
     LMS_BUCKET_NAME = 'lms.raw-files',
-    DEV_LMS_BUCKET_NAME = 'lms-vids';
+    DEV_LMS_BUCKET_NAME = 'lms-vids',
+    TERMS = {
+        first_term:{
+            title: 'first term',
+            start_date: Date.now(),
+            end_date: Date.now(),
+        },
+        second_term:{
+            title: 'second term',
+            start_date: new Date('2024-03-21T18:18:51.038Z'),
+            end_date: new Date('2024-06-21T18:18:51.038Z'),
+        },
+        third_term:{
+            title: 'third term',
+            start_date: Date.now(),
+            end_date: Date.now(),
+        }
+    };
+
