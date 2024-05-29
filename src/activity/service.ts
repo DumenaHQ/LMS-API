b8ead02dd5af18e2c81ee21417ecf7504b2e3f8c
BEZALEEL NWABIA <bezaleelnwabia@gmail.com>
2024-05-29 16:03:02 +0100

    Implemented suggested changes

--- src/activity/service.ts
+++ src/activity/service.ts
@@ -1,14 +1,29 @@
+import { Request } from 'express';
+import useragent from 'useragent'; // for more info, visit https://www.npmjs.com/package/useragent
+
 import { EActivityType } from './enum';
 import { Activity } from './model';
+import { handleError } from '../helpers/handleError';
+
 
 export const activityService = {
-    async create(userId: string, activityType:EActivityType, activityData: object){
-        const activity = await Activity.create({ user: userId, activityType, activityData });
-        return activity;
+    async create(req: Request, userId: string, activityType:EActivityType, activityData: object){
+        if (activityType === 'login') {
+            if (!req.headers) throw new handleError(400,'Something is wrong with this request, so we cant process it. Please verify you are sending this request with a header');
+            // access device information
+            const agent = useragent.lookup(req.headers['user-agent']);
+            const device = `DEVICE: ${agent.device.toString()}. OPERATING_SYSTEM: ${agent.os.toString()}. BROWSER: ${agent.toString()}`;
+            activityData = {
+                ...activityData,
+                meta:{
+                    device,
+                }
+            };
+        }
+        return Activity.create({ user: userId, activityType, activityData });
     },
 
     async list(match = {}){
-        const activities = await Activity.find(match);
-        return activities;
+        return Activity.find(match);
     },
 };
