59dd5d5dfec7da0c9475682ec6fa9f3d8e3761a2
BEZALEEL NWABIA <bezaleelnwabia@gmail.com>
2024-06-20 08:23:50 +0100

    added the scripti . to migrate existing schools to a plan based on their location (state)

--- src/subscription/controller.ts
+++ src/subscription/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { subscriptionService } from './service';
 import { send as sendResponse } from '../helpers/httpResponse';
+import { School } from '../user/models';
 
 export const listSubcriptions = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -27,6 +28,39 @@ export const updateSchoolSubscription = async (req: Request, res: Response, next
         const { coupon } = req.body;
         const schoolSubscription = await subscriptionService.updateSchoolSubscription(school_sub_id, { coupon });
         sendResponse(res, 201, 'School Subscription Updated', { schoolSubscription });
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+// This Block Of Code is for seeding purposes and will be removed after it is triggered once on production and staging, this comment will be removed also
+export const migrateExistingSchoolsToSubscription = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Get all schools
+        const schools = await School.find();
+
+        await Promise.all(schools.map(async (school) => {
+
+            const premiumStates = ['abuja', 'lagos'];
+            let subscription;
+            if (premiumStates.includes( String(school.resident_state).toLowerCase()) ){
+                subscription = await subscriptionService.findOne({
+                    title: { $regex: /^standard/i }
+                });
+            } else {
+                subscription = await subscriptionService.findOne({
+                    title: { $regex: /^basic/i }
+                });
+            }
+
+            if (subscription){
+                await subscriptionService.migrateSchoolToSubscription(school.id, subscription.id);
+            }
+            
+        }));
+
+        sendResponse(res, 201, 'Schools Migrating to Subscriptions');
     } catch (err) {
         next(err);
     }
--- src/subscription/routes.ts
+++ src/subscription/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { isAdmin, isAuthenticated } from '../middleware/verifyToken';
-import { create, listSubcriptions, updateSchoolSubscription } from './controller';
+import { create, listSubcriptions, migrateExistingSchoolsToSubscription, updateSchoolSubscription } from './controller';
 import validate, { schoolSubscriptionUpdateRules, subscriptionCreationRules } from '../middleware/validators/subscriptionValidator';
 
 export const router = Router();
@@ -11,3 +11,5 @@ router.get('/', listSubcriptions);
 router.post('/', isAuthenticated, isAdmin, subscriptionCreationRules(), validate, create);
 
 router.put('/school/:school_sub_id', isAuthenticated, isAdmin, schoolSubscriptionUpdateRules(), validate , updateSchoolSubscription);
+
+router.put('migrate-schools', migrateExistingSchoolsToSubscription)
