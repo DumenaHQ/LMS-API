4940a1fa80d559dee378b5c554ad7a4b5d339009
chibuzo <chibuzo.henry@gmail.com>
2024-08-06 11:04:54 +0100

    Extend learner support for programs

--- src/support/interface.ts
+++ src/support/interface.ts
@@ -1,23 +1,28 @@
 import { IUserModel } from '../user/models';
 import Class from '../class/model';
 import Course from '../course/model';
-export interface IAddSupportQuestion{
-question: string;
-user_id: string;
-class_id: string;
-course_id: string;
-lesson: any;
+import Program from '../program/model';
+
+export interface IAddSupportQuestion {
+    question: string;
+    user_id: string;
+    class_id: string;
+    program_id: string;
+    course_id: string;
+    lesson: any;
 }
 
-export interface IQuestion{
+export interface IQuestion {
     question: string;
     user: IUserModel;
     class: typeof Class;
+    program: typeof Program;
     course: typeof Course;
     lesson: any;
 }
+
 export interface IAddSupportComment {
-comment: string;
-user_id: string;
-question_id: string;
+    comment: string;
+    user_id: string;
+    question_id: string;
 }
