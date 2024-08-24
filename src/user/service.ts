54b56d45d817b2ad463c668fda3987aa5112c351
BEZALEEL NWABIA <bezaleelnwabia@gmail.com>
2024-08-25 00:38:35 +0100

    Updated the code to also enable search by name, email and username

--- src/user/service.ts
+++ src/user/service.ts
@@ -292,7 +292,7 @@ export const userService = {
 
 
     async listSchoolStudents(schoolId: string, queryParams: object = {}) {
-        const validParams = ['name', 'grade'];
+        const validParams = ['search', 'grade'];
         const validQueryParams: Record<string, any> = {};
         for (const [key, value] of Object.entries(queryParams)) {
             if (validParams.includes(key)) {
@@ -303,7 +303,13 @@ export const userService = {
             school: new mongoose.Types.ObjectId(schoolId),
             'user.deleted': false,
             ...(validQueryParams.grade && { grade: validQueryParams.grade }), // Added this: Filters learners by grade if the grade parameter is provided.
-            ...(validQueryParams.name && { 'user.fullname': { $regex: validQueryParams.name, $options: 'i' } }), // Updated this: Searches learners by fullname using a case-insensitive regular expression if the name parameter is provided.    
+            ...(validQueryParams.search && {
+                $or: [ // Added this: Searches learners by fullname, username, or email using a case-insensitive regular expression.
+                    { 'user.fullname': { $regex: validQueryParams.search, $options: 'i' } },
+                    { 'user.username': { $regex: validQueryParams.search, $options: 'i' } },
+                    { 'user.email': { $regex: validQueryParams.search, $options: 'i' } }
+                ]
+            }),
         };
         const learners = await this.list(criteria, 'learner');
         const grades: any = [];
