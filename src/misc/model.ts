5784ad6204694771ad964bccd18f52c493de438a
Chibuzo <chibuzo.henry@gmail.com>
2025-01-14 19:39:41 +0100

    Fix term schema

--- src/misc/model.ts
+++ src/misc/model.ts
@@ -8,19 +8,46 @@ const session = new Schema({
     },
     terms: {
         first_term: {
-            title: 'first term',
-            start_date: new Date(String(`${new Date().getFullYear()}-09-03T00:00:00.000Z`)),
-            end_date: new Date(`${new Date().getFullYear()}-12-23T00:00:00.000Z`),
+            title: {
+                type: String,
+                default: 'first term',
+            },
+            start_date: {
+                type: Date,
+                default: new Date(String(`${new Date().getFullYear()}-09-03T00:00:00.000Z`))
+            },
+            end_date: {
+                type: Date,
+                default: new Date(`${new Date().getFullYear()}-12-23T00:00:00.000Z`)
+            }
         },
         second_term: {
-            title: 'second term',
-            start_date: new Date(`${new Date().getFullYear() + 1}-01-03T00:00:00.000Z`),
-            end_date: new Date(`${new Date().getFullYear() + 1}-04-07T00:00:00.000Z`),
+            title: {
+                type: String,
+                default: 'second term',
+            },
+            start_date: {
+                type: Date,
+                default: new Date(`${new Date().getFullYear() + 1}-01-03T00:00:00.000Z`)
+            },
+            end_date: {
+                type: Date,
+                default: new Date(`${new Date().getFullYear() + 1}-04-07T00:00:00.000Z`)
+            }
         },
         third_term: {
-            title: 'third term',
-            start_date: new Date(`${new Date().getFullYear() + 1}-04-14T00:00:00.000Z`),
-            end_date: new Date(`${new Date().getFullYear() + 1}-07-20T00:00:00.000Z`),
+            title: {
+                type: String,
+                default: 'third term',
+            },
+            start_date: {
+                type: Date,
+                default: new Date(`${new Date().getFullYear() + 1}-04-14T00:00:00.000Z`)
+            },
+            end_date: {
+                type: Date,
+                default: new Date(`${new Date().getFullYear() + 1}-07-20T00:00:00.000Z`)
+            }
         }
     }
 })
